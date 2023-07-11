88c84a6a4dc609bd0b0687c06612e885209f4b90
TienDatn122 <tiendatduahau@gmail.com>
2023-07-12 06:54:25 +0700

    vonglapfor

---
+++ main.java
@@ -0,0 +1,13 @@
+package Vonglapfor;
+
+public class main {
+	public static void main(String[] args) {
+		int number = -99;
+				for (;number < 10; ) {
+					System.out.println(number);
+					number++;
+				}
+	}
+	
+
+}
